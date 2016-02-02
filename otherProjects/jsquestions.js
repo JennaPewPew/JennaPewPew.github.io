82afd8409ecfc5dcacb7ff892d080b29b228c237
JennaPewPew <piunnojenna@gmail.com>
2016-02-01 19:37:15 -0500

    hid the jumbotron on small devices

--- otherProjects/jsquestions.js
+++
@@ -1,9 +0,0 @@
-
-var degFahren = function(degCent){
-     return (9.0/5.0) * degCent + 32;
-}
-
-var celsius = prompt("Enter temperature in Centigrade");
-var fahren = degFahren(celsius);
-
-console.log("The temperature is" + "" + fahren + "F");
