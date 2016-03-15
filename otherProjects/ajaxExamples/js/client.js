e726c68b05910b35399905435a18b2de9b484ca3
JennaPewPew <piunnojenna@gmail.com>
2016-03-15 12:06:16 -0400

    added table

--- otherProjects/ajaxExamples/js/client.js
+++ otherProjects/ajaxExamples/js/client.js
@@ -1,12 +1,25 @@
+< table >
+  < tr > < th > < /th><th> </th > < th > < /th></tr >
+  < tr > < /tr> < /table>
 $(document).ready(function() {
   $("#getClients").on("click", function() {
 
     var url = "http://JennaPewPew.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json"
     $.getJSON(url, function(data) {
-        $.each(data, function(index, item){
-            $("#data").append(item.name)
+      var html = "<table class='table table-hover table-striped table-responsive'>" +
+        "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
 
-        })
+
+      $.each(data, function(index, item) {
+        html += "<tr>"
+        "<td>" + item.name + "</td>"
+        "<td>" + item.email + "</td>"
+        "<td>" + item.company + "</td>"
+        "</tr>"
+      })
+
+      html + "</table>";
+      $("#data").append(html);
     })
   })
 })
